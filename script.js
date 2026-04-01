233b68fa4fb88bcb0b8724a3811d68dd4d0066ab
mehak-garg <mehakgarg741@gmail.com>
2026-04-01 17:49:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  let prices=document.querySelectorAll(".prices");
+	let total_price=0;
+	for(let i=0;i<prices.length;i++)
+		{
+			total_price+=prices[i];
+		}
+	let last_row=document.createElement("tr");
+	last_row.innerHtml+="<td>total_price</td>";
+	document.table.appendChild(last_row);
 };
 
 getSumBtn.addEventListener("click", getSum);
